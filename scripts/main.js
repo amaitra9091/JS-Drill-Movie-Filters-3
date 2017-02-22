29c150e24e098868d367f3138abf2e85daa5aa17
Zachary Lee <w.zachary.lee@gmail.com>
2017-02-22 12:49:45 -0600

    removed unused jquery.js, fixed instructions for the drill, cleaned up the code, removed the solution

--- scripts/main.js
+++ scripts/main.js
@@ -1,32 +1,32 @@
 function each(list, callback) {
-    //write a function that iterates of the list and invokes the callback on
-    //each list item
-    list.forEach(callback);
+    /*
+    Write a function that iterates through the list and calls the callback on
+    each list item.
+    */
 }
 
 function filterBy (criteria) {
-    //return a function that returns a Boolean value
-        //if 
-            //the data matches the filter value return true
-        //else 
-            //return false
-    return function (data) {
-        return criteria === data.genre; 
-    };
+    /*
+    return a function that returns a Boolean value
+      if
+        the data matches the filter value return true
+      else
+        return false
+    */
 
 }
 
 function filter(list, fn) {
-    var outputList = [];
+    var outputList = []; //Array of the values that "pass" the check
     each(list, function(value) {
-        if(fn(value)) {
-            outputList.push(value);
-        }
+      /*
+      list is a list of data.
+      fn is a function that returns a boolean. You can think of the fn as a checking function.
+      Using each, iterate over each element in the list
+        if the list item passes the check:
+          then save the list item
+      */
     });
+    //Return all of the list items that pass the check
     return outputList;
-    //list is a list of data
-    //fn returns true or false. You can think of the fn as a Check
-    //using each, iterate over each element in the list
-    //if the list item passes the check, then save the list item
-    //retutn all of the list items that pass the check
 }
